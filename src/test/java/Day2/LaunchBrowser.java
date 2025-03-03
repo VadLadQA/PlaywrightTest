f6bcb3b4b47f8be9db021183649455c0138bb597
VadLadQA <ivadi555@gmail.com>
2025-03-03 18:48:58 +0400

    небольшой рефакторинг + комментарии в код

--- src/test/java/Day2/LaunchBrowser.java
+++ src/test/java/Day2/LaunchBrowser.java
@@ -10,18 +10,24 @@ public class LaunchBrowser {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         Page page = browser.newPage(); // page это страница браузера, 1 вкладка браузера
+
         page.navigate("https://ecommerce-playground.lambdatest.io/");
         Locator myAccount = page.locator("//a[contains(.,'My account')][@role='button']");
-        myAccount.hover();
+        myAccount.hover(); // наведение мышки на локатор
         page.click("//a[contains(.,'Login')]");
         assertThat(page).hasTitle("Account Login");
-        page.getByPlaceholder("E-Mail Address").type("test2@test.com");
-        page.getByPlaceholder("Password").type("password");
+        page.getByPlaceholder("E-Mail Address").type("test2@test.com"); // заполнить поле e-mail
+        page.getByPlaceholder("Password").type("password"); // заполнить поле password
         page.locator("//input[@value='Login']").click();
+
+        // проверка наличия и теста warning-а
         assertThat(page).hasTitle("Account Login");
         Locator warning = page.locator("[class='alert alert-danger alert-dismissible']  ");
+        String messageWarn = warning.textContent();
+        System.out.println(messageWarn);
         assertThat(warning).containsText("No match for E-Mail Address and/or Password.");
-        //допиши проверку наличия ошибки
+
+        // полное закрытие инициализированных элементов. В большинстве случаев достаточно только последнего - playwright.close().
         page.close();
         browser.close();
         playwright.close();
--- src/test/java/Day3/InteractWithInputs.java
+++ src/test/java/Day3/InteractWithInputs.java
@@ -10,6 +10,8 @@ public class InteractWithInputs {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
         Page page = browser.newPage();
+
+        // check the text of message
         page.navigate("https://www.lambdatest.com/selenium-playground/simple-form-demo");
         page.locator("input#user-message").type("Hello Man");
         page.locator("id=showInput").click();
